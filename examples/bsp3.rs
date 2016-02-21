7a1f525525df1c52d648825827fe9b16d963f5b2
robisys <robert@robisys.de>
2016-02-21 16:32:39 +0100

    Added files via upload

---
+++ examples/bsp3.rs
@@ -0,0 +1,52 @@
+extern crate curl;
+
+use curl::http;
+
+pub fn rbsp1(){
+
+  let purl = "http://raspberrypi.fritz.box";
+
+  let resp = http::handle()  
+  .post(purl, "this is the body")
+  .exec().unwrap();
+
+  println!("code={}; headers={:?}; body={:?}",
+    resp.get_code(), resp.get_headers(), resp.get_body());
+
+}
+
+
+pub fn rbsp2(){
+  let purl = "http://raspberrypi.fritz.box";
+
+  let resp = http::handle()
+    .get(purl).exec().unwrap();
+
+  println!("code={}; headers={:?}; body={:?}",
+    resp.get_code(), resp.get_headers(), resp.get_body());
+}
+
+
+pub fn rbsp4() {
+  let purl = "http://raspberrypi.fritz.box";
+
+  let resp = http::handle().get(purl).exec().unwrap();
+  println!("url: {}",purl);
+  
+  println!("code={}; headers={:?}; ",
+    resp.get_code(), resp.get_headers());
+
+}
+
+
+pub fn main() {
+
+ rbsp4();
+
+rbsp2();
+rbsp1();
+
+}
+
+
+
